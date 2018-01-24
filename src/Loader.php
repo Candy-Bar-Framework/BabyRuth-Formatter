fa8b18929a94796a3a6040e49537c821b245f14f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-23 19:15:38 -0500

    Create Loader.php

---
+++ src/Loader.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is a part of LaxovePHP.
+ *
+ * @author Nicholas English       <https://github.com/Nenglish7>.
+ * @author LaxovePHP Contributors <https://github.com/orgs/LaxovePHP/people>.
+ *
+ * @copyright 2017-2018 LaxovePHP.
+ *
+ * @link    <https://github.com/LaxovePHP/DotEnv> Github Repository.
+ * @license <https://github.com/LaxovePHP/DotEnv/blob/master/LICENSE> New BSD License.
+ */
+namespace LaxovePHP\DotEnv;
+/**
+ * Loader.
+ */
+class Loader implements LoaderInterface
+{
+    
+}
