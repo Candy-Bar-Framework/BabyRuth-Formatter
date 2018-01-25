7c22ad450b9e275fb8a7ba43a3fe01f5ac204912
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-24 21:22:56 -0500

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -13,4 +13,36 @@ namespace LaxovePHP\DotEnv;
  */
 class Utils
 {
+    /**
+     * depth();
+     *
+     * Get the depth of an array.
+     *
+     * @param array $testSubject The array to test.
+     *
+     * @throws InvalidArgumentException If $testSubject is not an array.
+     *
+     * @return int The array depth.
+     */
+    public static function depth($testSubject)
+    {
+        if (!is_array($testSubject)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Invalid data type passed for the 1st argument. Passed `%s`.',
+                htmlspecialchars(var_export(serialize($testSubject), true), ENT_QUOTES, 'UTF-8')
+            ));
+        }
+        $depth = 1;
+        if (empty($testSubject)) {
+            goto done;
+        }
+        foreach ($testSubject as $elementList) {
+            if (is_array($elementList)) {
+                $depth += self::depth($elementList);
+                break;
+            }
+        }
+        done:
+        return $depth
+    }
 }
