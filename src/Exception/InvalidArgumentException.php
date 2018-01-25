58619ee91612b147bb348de2784f7c3edba9bf49
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-24 21:32:36 -0500

    Create InvalidArgumentException.php

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is a part of LaxovePHP.
+ *
+ * @copyright 2017-2018 LaxovePHP.
+ *
+ * @link    <https://github.com/LaxovePHP/DotEnv> Github Repository.
+ * @license <https://github.com/LaxovePHP/DotEnv/blob/master/LICENSE> New BSD License.
+ */
+namespace LaxovePHP\DotEnv\Exception;
+/**
+ * InvalidArgumentException.
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
