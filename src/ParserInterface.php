30f024a00cfc4a710750f09d6297fd107d13d72f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-24 19:07:01 -0500

    Update ParserInterface.php

--- src/ParserInterface.php
+++ src/ParserInterface.php
@@ -1,1 +1,31 @@
-
+<?php
+/**
+ * This file is a part of LaxovePHP.
+ *
+ * @copyright 2017-2018 LaxovePHP.
+ *
+ * @link    <https://github.com/LaxovePHP/DotEnv> Github Repository.
+ * @license <https://github.com/LaxovePHP/DotEnv/blob/master/LICENSE> New BSD License.
+ */
+namespace LaxovePHP\DotEnv;
+/**
+ * ParserInterface.
+ */
+interface ParserInterface
+{
+    /**
+     * __construct().
+     *
+     * Set any avaliable options for the parser.
+     *
+     * @param mixed $options The list of avaliable options.
+     * @param bool $strict Should the parser run under strict mode.
+     *
+     * @throws UnexpectedValueException If the option is not a valid one.
+     * @throws InvalidArgumentException If the data type for that option is invalid.
+     * @throws DomainException          If the data for that option is still invalid.
+     *
+     * @return void.
+     */
+    function __construct($options = [], $strict);
+}
