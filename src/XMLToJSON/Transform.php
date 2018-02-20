bcc36f1a67104765cbbd638fb1b8ee072a1655ca
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-20 13:12:43 -0500

    Update Transform.php

--- src/XMLToJSON/Transform.php
+++ src/XMLToJSON/Transform.php
@@ -1,1 +1,22 @@
-
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of BabyRuth-XML-JSON-YAML-Parser.
+ *
+ * @link    <https://github.com/Nenglish7/SuperBCMS> Github repository.
+ * @license <https://github.com/Nenglish7/SuperBCMS/blob/master/LICENSE> BSD-3-Clause.
+ */
+namespace Genial\BabyRuth\XMLToJSON;
+/**
+ * use Traversable.
+ * use Genial\BabyRuth\Handler\JSONHandler.
+ */
+use Traversable;
+use Genial\BabyRuth\Handler\JSONHandler;
+/**
+ * Transform.
+ */
+class Transform extends JSONHandler implements TransformInterface
+{
+  
+}
